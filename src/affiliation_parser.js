2f7bea6a0fdd167337ccf7683fe7c512e87edaaf
piratical <ed.trager@gmail.com>
2024-02-07 21:50:11 -0500

    Many changes and refinements ...

--- src/affiliation_parser.js
+++ src/affiliation_parser.js
@@ -1,4 +1,4 @@
-//////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 //
 // affiliation_parser
 //
@@ -19,86 +19,142 @@ if(process.argv.length != 3){
 
 /////////////////////////////////////////////////////////////////////////////////////
 //
-// Campuses:
+// getCampus
 //
 /////////////////////////////////////////////////////////////////////////////////////
-//const campus = [
-//  { abbr:'aa', title:'The University of Michigan-Ann Arbor'},
-//  { abbr:'db', title:'The University of Michigan-Dearborn' },
-//  { abbr:'fl', title:'The University of Michigan-Flint'    }
-//];
+function getCampus(affiliationString){
 
-/////////////////////////////////////////
-//
-// titles:
-//
-/////////////////////////////////////////
-const title = [
-  { key:'Professsor emerit(us|a)', name:'Professor Emeritus/a' },
-  { key:'Assistant Professor', name:'Assistant Professor' },
-  { key:'Associate Professor', name:'Associate Professor' },
-  { key:'Professor', name:'Professor' },
-  { key:'Lecturer', name:'Lecturer' }
-];
-
-/////////////////////////////////////////////////////////////////////////////////////
-//
-// Schools and college by campus abbreviation:
-//
-/////////////////////////////////////////////////////////////////////////////////////
-const campus = [
-  { abbr:'aa',
-    name:'Ann Arbor',
-    school:[
-      { key:'College of Architecture (and|&) Urban Planning', name:'Taubman College of Architecture and Urban Planning' },
-      { key:'School of Art (and|&) Design', name:'Stamps School of Art and Design' },
-      { key:'School of Business', name:'Ross School Business' },
-      { key:'School of Education', name:'Marsal Family School of Education' },
-      { key:'School of Information', name:'School of Information' },
-      { key:'School of Kinesiology', name:'School of Kinesiology' },
-      { key:'College of Engineering', name:'College of Engineering' },
-      { key:'College of Literature, Science,? and the Arts', name:'College of Literature, Science and the Arts' },
-      { key:'College of Pharmacy', name:'College of Pharmacy' },
-      { key:'Institute for Social Research', name:'Institute for Social Research' },
-      { key:'Law School', name:'Law School' },
-      { key:'Medical School|University of Michigan Hospitals|Michigan Medicine', name:'Medical School' },
-      { key:'National Center for Institutional Diversity', name:'National Center for Institutional Diversity' },
-      { key:'Office of the Vice President for Research', name:'Office of the Vice President for Research' },
-      { key:'Rackham( School of Graduate Studies)?', name:'Rackham School of Graduate Studies' },
-      { key:'School for Environment (and|&) Sustainability', name:'School for Environment and Sustainability' },
-      { key:'School of Dentistry', name:'School of Dentistry' },
-      { key:'School of Education', name:'School of Education' },
-      { key:'School of Information', name:'School of Information' },
-      { key:'School of Music, Theat(re|er) (and|&) Dance', name:'School of Music, Theatre and Dance' },
-      { key:'School of Nursing', name:'School of Nursing' },
-      { key:'School of Pharmacy', name:'School of Pharmacy' },
-      { key:'School of Public Health', name:'School of Public Health' },
-      { key:'School of Public Policy', name:'School of Public Policy' },
-      { key:'School of Social Work', name:'School of Social Work' },
-      { key:'University Library', name: 'University Library' }
-    ]
-  },
-  { abbr:'db',
-    name:'Dearborn',
-    school:[    
-      { key:'College of Arts, Sciences (and|&) Letters', name:'College of Arts, Sciences and Letters' },
-      { key:'College of Business', name:'College of Business' },
-      { key:'College of Education, Health,? (and|&) Human Services', name:'College of Education, Health and Human Services' },
-      { key:'College of Engineering (and|&) Computer Science', name:'College of Engineering and Computer Science' }
-    ],
-  },
-  { abbr:'fl',
-    name:'Flint',
-    school:[
-      { key:'College of Arts (and|&) Sciences|College of Arts,? Sciences (and|&) Education', 
-        name:'College of Arts, Sciences and Education' },
-      { key: 'School of Management', name:'School of Management' },
-      { key: 'College of Health Sciences', name:'College of Health Sciences' },
-      { key: 'School of Nursing', name:'School of Nursing' },
-      { key:'College of Innovation (and|&) Technology', name:'College of Innovation and Technology' },
-     ]
+  affiliationString = affiliationString.trim();
+  if(!affiliationString || affiliationString==='#N/A'){
+    return null;
   }
-];
+   
+  const campus = [
+    { abbr:'aa', key:'The University of Michigan-Ann Arbor', name:'Ann Arbor' },
+    { abbr:'db', key:'The University of Michigan-Dearborn' , name:'Dearborn'  },
+    { abbr:'fl', key:'The University of Michigan-Flint'    , name:'Flint'     }
+  ];
+  
+  for(let i=0;i<campus.length;i++){
+    if(affiliationString.match(campus[i].key)){
+      return campus[i];
+    }
+  }
+  // No match then default to Ann Arbor:
+  return campus[0];
+}
+
+/////////////////////////////////////////
+//
+// getFacultyTitle
+//
+/////////////////////////////////////////
+function getFacultyTitle(affiliationString){
+  
+  affiliationString = affiliationString.trim();
+  if(!affiliationString || affiliationString==='#N/A'){
+    return { is_faculty:null, title: null };
+  }
+   
+  const title = [
+    { key:'Professsor emerit(us|a)', name:'Professor Emeritus/a' },
+    { key:'Assistant Professor', name:'Assistant Professor' },
+    { key:'Associate Professor', name:'Associate Professor' },
+    { key:'Professor', name:'Professor' },
+    { key:'Lecturer', name:'Lecturer' }
+  ];
+
+  for(let i=0;i<title.length;i++){
+    if( affiliationString.match(title[i].key)){
+      return { is_faculty:true, title: title[i].name };
+    }
+  }
+  // No match:
+  return { is_faculty:false, title: null };
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// getSchoolOnCampus(campus,affiliationString)
+//
+/////////////////////////////////////////////////////////////////////////////////////
+function getSchoolOnCampus(campus,affiliationString){
+
+  affiliationString = affiliationString.trim();
+  if( !affiliationString || affiliationString==='#N/A'){
+    return null;
+  }
+  
+  const schoolMap = {
+    aa:{
+      abbr:'AA',
+      name:'Ann Arbor',
+      school:[
+        { key:'College of Architecture (and|&) Urban Planning', name:'Taubman College of Architecture and Urban Planning' },
+        { key:'School of Art (and|&) Design', name:'Stamps School of Art and Design' },
+        { key:'School of Business', name:'Ross School Business' },
+        { key:'School of Education', name:'Marsal Family School of Education' },
+        { key:'School of Information', name:'School of Information' },
+        { key:'School of Kinesiology', name:'School of Kinesiology' },
+        { key:'College of Engineering', name:'College of Engineering' },
+        { key:'College of Literature, Science,? and the Arts', name:'College of Literature, Science and the Arts' },
+        { key:'College of Pharmacy', name:'College of Pharmacy' },
+        { key:'Institute for Social Research', name:'Institute for Social Research' },
+        { key:'Law School', name:'Law School' },
+        { key:'UMHS|Medical School|University of Michigan Hospitals|Michigan Medicine', name:'Medical School' },
+        { key:'National Center for Institutional Diversity', name:'National Center for Institutional Diversity' },
+        { key:'Office of the Vice President for Research', name:'Office of the Vice President for Research' },
+        { key:'Rackham( School of Graduate Studies)?', name:'Rackham School of Graduate Studies' },
+        { key:'SEAS|School for Environment (and|&) Sustainability', name:'School for Environment and Sustainability' },
+        { key:'School of Dentistry', name:'School of Dentistry' },
+        { key:'School of Education', name:'School of Education' },
+        { key:'School of Information', name:'School of Information' },
+        { key:'School of Music, Theat(re|er) (and|&) Dance', name:'School of Music, Theatre and Dance' },
+        { key:'School of Nursing', name:'School of Nursing' },
+        { key:'School of Pharmacy', name:'School of Pharmacy' },
+        { key:'School of Public Health', name:'School of Public Health' },
+        { key:'School of Public Policy', name:'School of Public Policy' },
+        { key:'School of Social Work', name:'School of Social Work' },
+        { key:'University Library', name: 'University Library' }
+     ]
+    },
+    db:{
+      abbr:'DB',
+      name:'Dearborn',
+      school:[    
+        { key:'College of Arts, Sciences (and|&) Letters', name:'College of Arts, Sciences and Letters' },
+        { key:'College of Business', name:'College of Business' },
+        { key:'College of Education, Health,? (and|&) Human Services', name:'College of Education, Health and Human Services' },
+        { key:'College of Engineering (and|&) Computer Science', name:'College of Engineering and Computer Science' }
+      ]
+    },
+    fl:{
+      abbr:'FL',
+      name:'Flint',
+      school:[
+        { key:'College of Arts (and|&) Sciences|College of Arts,? Sciences (and|&) Education', 
+          name:'College of Arts, Sciences and Education' },
+        { key: 'School of Management', name:'School of Management' },
+        { key: 'College of Health Sciences', name:'College of Health Sciences' },
+        { key: 'School of Nursing', name:'School of Nursing' },
+        { key:'College of Innovation (and|&) Technology', name:'College of Innovation and Technology' },
+      ]
+    }
+  };
+
+  const map = schoolMap[campus.abbr]
+  if( map === undefined ){
+    return null;
+  }
+  
+  for(let i=0;i<map.school.length;i++){
+    if(affiliationString.match( map.school[i].key )){
+      return map.school[i].name;
+    }
+  }
+  // no match:
+  return null;
+}
 
 ////////////////////////////////////////////////////////////
 // 
@@ -112,92 +168,6 @@ const unit = [
   '',
 ];
 
-/////////////////////////////////////////////////
-//
-// arrayToRegexOptionGroup
-//
-// NOTE: This also sorts the array in place
-//       in order to put longest options first
-//       which is what you will want for proper
-//       behavior in your regular expression.
-//
-/////////////////////////////////////////////////
-function arrayToRegexOptionGroup(arr){
-  // Sort to longest strings first.
-  // Since we are sorting in descending order by
-  // length, it seems reasonable to also do
-  // the nested alphabetic sort in reverse order too.
-  // This nested sort is not critical, but makes it a
-  // little bit easier for human eyes:
-  arr.sort( (a,b)=>{
-    return b.length-a.length || b.localeCompare(a);
-  });
-  return '('+arr.join('|')+')';
-}
-
-// Turn the POS array into a regexp fragment with the OR operator '|' between each option:
-//const posAbbr = arrayToRegexOptionGroup(abbr);
-//console.log(posAbbr);
-
-// REGEXPS FOR DATA COLLECTION:
-//const peelPattern = new RegExp(`(.+) ${posAbbr}\. (.+)$`);
-//const wppPattern  = new RegExp(`^${wppAbbr}\. (.+)$`);
-
-
-// Accumulator class to save data in for multiline reads:
-class Accumulator {
-  // constructor
-  constructor(entry,pos,def){
-    this.entry=entry;
-    this.pos=pos;
-    this.def=def;
-  }
-
-  // reset:
-  reset(){
-    this.entry='';
-    this.pos  ='';
-    this.def  ='';
-  }
-  
-  // set
-  set(entry='',pos='',def=''){
-    this.entry=entry;
-    this.pos=pos;
-    this.def=def;
-  }
-
-  // addToDefinition
-  addToDefinition(s){
-    s = s.trim();
-    if( s.length === 0 ){
-      return;
-    }
-    if(this.def && this.def.length && this.def.substr(this.def.length-1)!=' '){
-      this.def += ' ';
-    }
-    this.def += s;
-  }
-  
-  // write
-  write(){
-    if(this.entry || this.pos || this.def){
-      console.log(`${this.entry}\t${this.pos}\t${this.def}`);
-    }
-  }
-  
-  // copy (copy constructor):
-  copy(){
-    return new Accumulator(this.entry,this.pos,this.def);
-  }
-}
-
-// Global reusable container to accumulate the parts of an entry:
-const accumulator = new Accumulator();
-
-// On the first pass through, put all entries into this:
-const accumulatorArray = [];
-
 const inputFile = process.argv[2];
 var lineReader = require('readline').createInterface({
   input: require('fs').createReadStream(inputFile)
@@ -209,27 +179,20 @@ var lineReader = require('readline').createInterface({
 //
 ////////////////////////////////////////////
 function processLine(line){
-  console.log('==============');
-  for(let i=0;i<campus.length;i++){
-    for(let j=0;j<campus[i].school.length;j++){
-      const pattern = campus[i].school[j].key;
-      //console.log(`TESTING ${item}...`);
-      if(line.match(pattern)){
-        // Try to nab title too:
-        let is_faculty=false;
-        for(let k=0;k<title.length;k++){
-          if(line.match(title[k])){
-            is_faculty=true;
-            break;
-          }
-        }
-        console.log(`{ 'faculty_staff':'${is_faculty?'faculty':'staff'}' 'campus':'${campus[i].abbr}', school:'${campus[i].school[j].name}', 'line':'${line}' }`);
-        return; 
-      }
-    }
-  }
-  console.log(`NO MATCH: ${line}`);
-};
+
+  const data={};
+  data.campus = getCampus(line);
+  data.school = getSchoolOnCampus(data.campus,line);
+  const facultyStaffInfo = getFacultyTitle(line);
+  data.is_faculty = facultyStaffInfo.is_faculty;
+
+  console.log('==============')
+  console.log(line);
+  console.log(` +- campus: ${ data.campus ? data.campus.name : null }`);
+  console.log(`      +- school: ${data.school}`);
+  console.log(`           +- type: ${ data.is_faculty===null ? null : data.is_faculty ? 'faculty' : 'staff' }`);
+} 
+
 
 /////////////////////////////////////
 //
@@ -238,21 +201,4 @@ function processLine(line){
 /////////////////////////////////////
 let count=0;
 lineReader.on('line',processLine);
-
-//lineReader.on('line', function (line) {
-  //console.log('==============');
-  //const tokens = line.split(', ');
-  //console.log(tokens);
-//  school.
-//  const matches = line.match(/(College|School).*/);
-//  if(matches){
-//    console.log(matches[0]);
-//  }
-//});
-
-  //
-  // JSONIFIED OUTPUT:
-  //
-  //console.log(JSON.stringify(accumulatorArray,null,1));
-  //console.log(accumulatorArray.length);
 
